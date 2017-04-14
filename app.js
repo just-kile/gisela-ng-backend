d17740622ed186bf591054f93eb1999a4b37a6f1
Philipp Nürnberger <pihl@notsecu.re>
2017-04-14 14:19:15 +0000

    deploy on notsecu.re server

--- app.js
+++ app.js
@@ -8,16 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-
 var routes = require("./routes/routes.js")(app);
-
-
-
-
-//var server = https.createServer(options, app).listen(port, function(){
-//  console.log("Express server listening on port " + port);
-//});
-
 
 var server = app.listen(3000, function () {
     console.log("Listening on port %s...", server.address().port);
